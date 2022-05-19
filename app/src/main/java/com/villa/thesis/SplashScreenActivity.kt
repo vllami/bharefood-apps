55ff930088f755012fb98ff5fbfbb2b9da190e14
Villa Mukti Indriyanto <villamuktiindriyanto@gmail.com>
2022-05-19 21:37:23 +0700

    Finishing SplashScreenActivity and Added OnboardingActivity

--- app/src/main/java/com/villa/thesis/SplashScreenActivity.kt
+++ app/src/main/java/com/villa/thesis/SplashScreenActivity.kt
@@ -1,11 +1,35 @@
 package com.villa.thesis
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.villa.thesis.databinding.ActivitySplashScreenBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var activitySplashScreenBinding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+
+        activitySplashScreenBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+
+        setContentView(activitySplashScreenBinding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            Intent(this, OnboardingActivity::class.java).also { toOnboarding ->
+                startActivity(toOnboarding)
+                finish()
+            }
+        }, SPLASH_DELAY.toLong())
     }
+
+    companion object {
+        const val SPLASH_DELAY = 3_000
+    }
+
 }
