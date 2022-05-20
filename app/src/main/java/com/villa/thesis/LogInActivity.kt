254c49bda3259b268e16a88298d13b38f9d434fb
Villa Mukti Indriyanto <villamuktiindriyanto@gmail.com>
2022-05-20 10:49:08 +0700

    Finishing SignUpActivity, LogInActivity Binding and Added ForgotPasswordActivity, HomeActivity

--- app/src/main/java/com/villa/thesis/LogInActivity.kt
+++ app/src/main/java/com/villa/thesis/LogInActivity.kt
@@ -1,11 +1,82 @@
 package com.villa.thesis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import com.villa.thesis.databinding.ActivityLogInBinding
 
 class LogInActivity : AppCompatActivity() {
+
+    private lateinit var activityLogInBinding: ActivityLogInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+
+        activityLogInBinding = ActivityLogInBinding.inflate(layoutInflater)
+
+        activityLogInBinding.apply {
+            setContentView(root)
+
+            toolbarLogIn.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            etLogInPassword.apply {
+                setOnEditorActionListener { _, editorInfo, _ ->
+                    when (editorInfo) {
+                        EditorInfo.IME_ACTION_DONE -> clearFocus()
+                    }
+
+                    false
+                }
+            }
+
+            tvLogInSignUpTextButton.setOnClickListener {
+                Intent(this@LogInActivity, SignUpActivity::class.java).also { fromLogInToSignUp ->
+                    fromLogInToSignUp.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    startActivity(fromLogInToSignUp)
+
+                    finish()
+                }
+            }
+
+            tvLogInForgotPassword.setOnClickListener {
+                Intent(this@LogInActivity, ForgotPasswordActivity::class.java).also { fromLogInToForgotPassword ->
+                    fromLogInToForgotPassword.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    startActivity(fromLogInToForgotPassword)
+                }
+            }
+
+            showProgress(false)
+
+            btnLogIn.setOnClickListener {
+                showProgress(true)
+
+                Intent(this@LogInActivity, HomeActivity::class.java).also { fromLogInToHome ->
+                    fromLogInToHome.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(fromLogInToHome)
+
+                    finish()
+                }
+            }
+        }
     }
+
+    private fun showProgress(state: Boolean) {
+        activityLogInBinding.apply {
+            when {
+                state -> {
+                    btnLogIn.visibility = View.INVISIBLE
+                    cvLogInLoading.visibility = View.VISIBLE
+                }
+                else -> {
+                    btnLogIn.visibility = View.VISIBLE
+                    cvLogInLoading.visibility = View.INVISIBLE
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/villa/thesis/SignUpActivity.kt
+++ app/src/main/java/com/villa/thesis/SignUpActivity.kt
@@ -1,11 +1,84 @@
 package com.villa.thesis
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import androidx.appcompat.app.AppCompatActivity
+import com.villa.thesis.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var activitySignUpBinding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        activitySignUpBinding = ActivitySignUpBinding.inflate(layoutInflater)
+
+        activitySignUpBinding.apply {
+            setContentView(root)
+
+            toolbarSignUp.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            etSignUpPassword.apply {
+                setOnEditorActionListener { _, editorInfo, _ ->
+                    when (editorInfo) {
+                        EditorInfo.IME_ACTION_DONE -> clearFocus()
+                    }
+
+                    false
+                }
+            }
+
+            tvSignUpLogInTextButton.setOnClickListener {
+                Intent(this@SignUpActivity, LogInActivity::class.java).also { toLogIn ->
+                    toLogIn.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    startActivity(toLogIn)
+
+                    finish()
+                }
+            }
+
+            btnSignUp.setOnClickListener {
+                Intent(this@SignUpActivity, HomeActivity::class.java).also { toHome ->
+                    toHome.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(toHome)
+
+                    finish()
+                }
+            }
+
+            showControl(false)
+
+            btnSignUp.setOnClickListener {
+                showControl(true)
+
+                Intent(this@SignUpActivity, HomeActivity::class.java).also { toHome ->
+                    toHome.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(toHome)
+
+                    finish()
+                }
+            }
+        }
     }
+
+    private fun showControl(state: Boolean) {
+        activitySignUpBinding.apply {
+            when {
+                state -> {
+                    btnSignUp.visibility = View.INVISIBLE
+                    cvSignUpLoading.visibility = View.VISIBLE
+                }
+                else -> {
+                    btnSignUp.visibility = View.VISIBLE
+                    cvSignUpLoading.visibility = View.INVISIBLE
+                }
+            }
+        }
+    }
+
 }
