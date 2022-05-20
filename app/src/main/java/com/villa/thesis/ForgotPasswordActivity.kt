945c7663804a1ab5484786624ab12eb1e3135d36
Villa Mukti Indriyanto <villamuktiindriyanto@gmail.com>
2022-05-20 19:47:44 +0700

    Finishing ForgotPasswordActivity Binding

--- app/src/main/java/com/villa/thesis/ForgotPasswordActivity.kt
+++ app/src/main/java/com/villa/thesis/ForgotPasswordActivity.kt
@@ -2,10 +2,71 @@ package com.villa.thesis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import com.villa.thesis.databinding.ActivityForgotPasswordBinding
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var activityForgotPasswordBinding: ActivityForgotPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+
+        activityForgotPasswordBinding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+
+        activityForgotPasswordBinding.apply {
+            setContentView(root)
+
+            toolbarForgotPassword.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            etForgotPasswordEmail.apply {
+                setOnEditorActionListener { _, editorInfo, _ ->
+                    when (editorInfo) {
+                        EditorInfo.IME_ACTION_DONE -> clearFocus()
+                    }
+
+                    false
+                }
+            }
+
+            showControl(false)
+
+            btnForgotPassword.setOnClickListener {
+                showControl(true)
+
+                Snackbar.make(it, getString(R.string.forgot_password_snackbar), Snackbar.LENGTH_LONG).apply {
+                    anchorView = it
+
+                    setTextColor(getColor(R.color.white))
+                    setBackgroundTint(getColor(R.color.dark_gray))
+                    setActionTextColor(getColor(R.color.white))
+
+                    animationMode = BaseTransientBottomBar.ANIMATION_MODE_SLIDE
+
+                    show()
+                }
+            }
+        }
     }
+
+    private fun showControl(state: Boolean) {
+        activityForgotPasswordBinding.apply {
+            when {
+                state -> {
+                    btnForgotPassword.visibility = View.INVISIBLE
+                    cvForgotPasswordLoading.visibility = View.VISIBLE
+                }
+                else -> {
+                    btnForgotPassword.visibility = View.VISIBLE
+                    cvForgotPasswordLoading.visibility = View.INVISIBLE
+                }
+            }
+        }
+    }
+
 }
