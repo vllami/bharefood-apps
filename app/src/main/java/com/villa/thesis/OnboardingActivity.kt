7a34d487437b1a4a44fd63de9ef9aff4d49ab0cb
Villa Mukti Indriyanto <villamuktiindriyanto@gmail.com>
2022-05-20 09:05:05 +0700

    Finishing OnboardingActivity and Added SignUpActivity, LogInActivity

--- app/src/main/java/com/villa/thesis/OnboardingActivity.kt
+++ app/src/main/java/com/villa/thesis/OnboardingActivity.kt
@@ -1,11 +1,34 @@
 package com.villa.thesis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.villa.thesis.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var activityOnboardingBinding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+
+        activityOnboardingBinding = ActivityOnboardingBinding.inflate(layoutInflater)
+
+        activityOnboardingBinding.apply {
+            setContentView(root)
+
+            btnOnboardingLogIn.setOnClickListener {
+                Intent(this@OnboardingActivity, LogInActivity::class.java).also { fromOnboardingToLogIn ->
+                    startActivity(fromOnboardingToLogIn)
+                }
+            }
+
+            btnOnboardingSignUp.setOnClickListener {
+                Intent(this@OnboardingActivity, SignUpActivity::class.java).also { fromOnboardingToSignUp ->
+                    startActivity(fromOnboardingToSignUp)
+                }
+            }
+        }
     }
+
 }
